fa2625cb3d3a485a41fee4b8d9524e9c9274ebdf
root <root@mail.com>
2016-04-29 20:06:30 +0000

    lab DDS added

--- src/Procedural/MainDDS.java
+++ src/Procedural/MainDDS.java
@@ -2,9 +2,18 @@ package Procedural;
 
 public class MainDDS {
     public static void main(String[] args) {
-        Node iter0 = MyDDS.generateIter0(4);
+//        Node iter0 = MyDDS.generateIter0(8);
+        Node iter1 = MyDDS.generateIter1(3);
+//        Node recu0 = MyDDS.generateRecu0(8);
+//        Node recu1 = MyDDS.generateRecu1(9);
 
-        System.out.println(MyDDS.toStringRec(iter0));
-//        MyDDS.showRecu(tail);
+//        System.out.println(MyDDS.toStringIter(iter0));
+        System.out.println(MyDDS.toStringRec(iter1));
+//        System.out.println(MyDDS.toStringIter(recu0));
+//        System.out.println(MyDDS.toStringRec(recu1));
+//        System.out.println("Size = " + MyDDS.getLength(iter1));
+//        System.out.println("Sum = " + MyDDS.getSum(iter1));
+        System.out.println("Max = " + MyDDS.getMax(iter1));
+//        System.out.println("Min = " + MyDDS.getMin(iter1));
     }
 }
--- src/Procedural/MyDDS.java
+++ src/Procedural/MyDDS.java
@@ -4,10 +4,27 @@ public class MyDDS {
 
     public static Node generateIter0(int size) {
         Node result = null;
-        for (int i = 0; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             result = new Node(i, result);
         }
         return result;
+    }
+
+    public static Node generateIter1(int size) {
+        if (size == 0) {
+            return null;
+        }
+        Node tail = new Node(size - 1, null);
+        Node head = tail;
+        for (int i = size - 1; i > 0; i--) {
+            head.next = new Node(i - 1, null);
+            head = head.next;
+        }
+        return tail;
+    }
+
+    public static Node generateRecu0(int size) {
+        return size > 0 ? new Node(size - 1, generateRecu0(--size)) : null;
     }
 
     public static String toStringIter(Node node) {
@@ -22,5 +39,59 @@ public class MyDDS {
 
     public static String toStringRec(Node node) {
         return node == null ? "null" : "(" + node.value + ")->" + toStringRec(node.next);
+    }
+
+    public static int getLength(Node node) {
+//        int size = 0;
+//        while (node != null) {
+//            size = size + 1;
+//            node = node.next;
+//        }
+//        return size;
+        return node == null ? 0 : 1 + getLength(node.next);
+    }
+
+    public static int getSum(Node node) {
+//        int sum = 0;
+//        while (node != null) {
+//            sum += node.value;
+//            node = node.next;
+//        }
+//        return sum;
+        return node == null ? 0 : node.value + getSum(node.next);
+    }
+
+    public static int getMax(Node node) {
+//        if (node == null) {
+//            throw new IllegalArgumentException("arg can not be null");
+//        }
+//        int max = node.value;
+//        while (node != null) {
+//            max = node.value > max ? node.value : max;
+//            node = node.next;
+//        }
+//        return max;
+
+        if (node.value > getMax(node.next)) {
+            return node.value;
+        } else {
+            return node.value > node.next.value ? node.value : node.next.value;
+        }
+    }
+
+    public static int getMin(Node node) {
+        if (node == null) {
+            throw new IllegalArgumentException("arg can not be null");
+        }
+        int min = node.value;
+        while (node != null) {
+            min = node.value < min ? node.value : min;
+            node = node.next;
+        }
+        return min;
+    }
+
+    public static Node copy(Node node) {
+        return node == null ? null : new Node(node.value, node.next);
     }
 }
