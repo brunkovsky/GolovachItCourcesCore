518364478faacd102984672ae510dfc1fd74ceaf
root <root@mail.com>
2016-04-30 04:34:52 +0000

    lab2 DDS added

--- src/Procedural/MainDDS.java
+++ src/Procedural/MainDDS.java
@@ -1,7 +1,11 @@
 package Procedural;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainDDS {
     public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
 //        Node iter0 = MyDDS.generateIter0(8);
         Node iter1 = MyDDS.generateIter1(8);
 //        Node recu0 = MyDDS.generateRecu0(8);
@@ -11,9 +15,11 @@ public class MainDDS {
         System.out.println(MyDDS.toStringRec(iter1));
 //        System.out.println(MyDDS.toStringIter(recu0));
 //        System.out.println(MyDDS.toStringRec(recu1));
-        System.out.println("Len = " + MyDDS.getLength(iter1));
-        System.out.println("Sum = " + MyDDS.getSum(iter1));
-        System.out.println("Max = " + MyDDS.getMax(iter1));
-        System.out.println("Min = " + MyDDS.getMin(iter1));
+//        System.out.println("Len = " + MyDDS.getLength(iter1));
+//        System.out.println("Sum = " + MyDDS.getSum(iter1));
+//        System.out.println("Max = " + MyDDS.getMax(iter1));
+//        System.out.println("Min = " + MyDDS.getMin(iter1));
+        iter1.remove(1);
+        System.out.println(MyDDS.toStringRec(iter1));
     }
 }
--- src/Procedural/Node.java
+++ src/Procedural/Node.java
@@ -8,4 +8,21 @@ public class Node {
         this.value = value;
         this.next = next;
     }
+
+    public void remove(int element) {
+//        Node node = this;
+//        while (--element > 0) {
+//            node = node.next;
+//        }
+        Node node = this;
+        node = node.next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + this.next.value +
+                '}';
+    }
 }
